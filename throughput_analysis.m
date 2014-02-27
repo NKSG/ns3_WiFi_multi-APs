6dfdc1fef5307f0d7a82a84553323bf918815a65
nalvin <alvin1019127@gmail.com>
2014-02-27 11:00:58 +0800

    Update throughput_analysis.m

--- throughput_analysis.m
+++ throughput_analysis.m
@@ -8,13 +8,15 @@ function throughput = throughput_analysis(n, datarate)
     P_s = n*tau*(1-tau)^(n-1)/(1-(1-tau)^n);
     P_t = 1-(1-tau)^n;
     
-    % E_L = 8184;
-    E_L = 1024*8;
-    T_sifs = 10;
-    T_difs = 10+20*2;
-    % 272+128=header, 112+128=ack
-    T_S_BAS = (E_L+(272+128))/datarate+T_sifs+T_difs+(112+128);
-    T_C_BAS = (E_L+(272+128))/datarate+T_difs;
+    E_L = 1024*8; % bit
+    T_sifs = 10; %us
+    T_difs = 10+20*2; %us
+    preamble = 192; %us
+    Mac_header = 272; %bit
+    ack_size = 112; % bit
+% Notice that data using datarate to transmit, control using 1 Mbps to transmit
+    T_S_BAS = preamble+(E_L+(Mac_header+preamble))/datarate+T_sifs+T_difs+(preamble+ack_size);
+    T_C_BAS = preamble+(E_L+(Mac_header+preamble))/datarate+T_difs;
     T_S = T_S_BAS;
     T_C = T_C_BAS;
     sigma = 20;
